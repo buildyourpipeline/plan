eb5b337a45a4707cf7a447a541cd8af12a3751d4
buildyourpipeline <48075988+buildyourpipeline@users.noreply.github.com>
2019-02-28 14:35:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 // app is running!
 app.get('/', function(req, res) {
+    npm start;
     res.send('Hello from NodeJS  at ' + new Date());
 });
 app.listen(8080, ip);
